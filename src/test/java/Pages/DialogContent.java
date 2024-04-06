e86dc42abb72aca56fb486956612c56b2acce016
elcnburak <elcnburak@hotmail.com>
2024-04-06 20:56:15 +0100

    ParentPage added

--- src/test/java/Pages/DialogContent.java
+++ src/test/java/Pages/DialogContent.java
@@ -1,6 +1,55 @@
 package Pages;
 
-public class DialogContent {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static Utilities.GWD.getDriver;
+
+public class DialogContent extends ParentPage{
+
+    public DialogContent() {
+        PageFactory.initElements(getDriver(),this);
+    }
+
+    @FindBy(xpath="")
+    public WebElement firstName;
+
+    @FindBy(xpath="")
+    public WebElement lastName;
+
+    @FindBy(xpath="")
+    public WebElement address;
+
+    @FindBy(xpath="")
+    public WebElement city;
+
+    @FindBy(xpath="")
+    public WebElement state;
+
+    @FindBy(xpath="")
+    public WebElement zipCode;
+    @FindBy(xpath="")
+    public WebElement zipCode;
+
+    @FindBy(xpath="")
+    public WebElement phone;
+
+    @FindBy(xpath="")
+    public WebElement SSN;
+
+    @FindBy(xpath="")
+    public WebElement userName;
+
+    @FindBy(xpath="")
+    public WebElement password;
+
+    @FindBy(xpath="")
+    public WebElement confirm;
+
+    @FindBy(xpath="")
+    public WebElement registerButton;
+
 
 
 
