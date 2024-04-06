2cedc8f67a67ed944e40c77ccad9bc2786680fdf
elcnburak <elcnburak@hotmail.com>
2024-04-06 20:51:42 +0100

    ParentPage added

---
+++ src/test/java/Pages/ParentPage.java
@@ -0,0 +1,47 @@
+package Pages;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+import static Utilities.GWD.*;
+
+public class ParentPage {
+
+
+    public WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+
+    public void myClick(WebElement element){
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        scrollToElement(element);
+        element.click();
+    }
+
+    public void mySendKeys(WebElement element, String text) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+        scrollToElement(element);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    //elemente kadar scroll yap
+    public void scrollToElement(WebElement element){
+        JavascriptExecutor js=(JavascriptExecutor) getDriver();
+        js.executeScript("arguments[0].scrollIntoView();", element);
+    }
+
+    public void verifyContainsText(WebElement element, String value){
+        wait.until(ExpectedConditions.textToBePresentInElement(element,value));
+        Assert.assertTrue(element.getText().toLowerCase().contains(value.toLowerCase()));
+
+        new Actions(getDriver()).sendKeys(Keys.ESCAPE).build().perform();
+        //sayfaya ESC tuşu gönderildi
+    }
+
+}
